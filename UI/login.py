2e7a6d623b94c9945343cc865952d5a8127e709a
Dario Roman <darioroman2001@gmail.com>
2022-06-09 22:41:00 -0400

    move from tkinter to kivy.

--- UI/login.py
+++ UI/login.py
@@ -1,36 +1,51 @@
-from tkinter import *
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen, ScreenManager
 
+class LoginLayout(GridLayout):
+    def __init__(self, **kwargs,):
+        super(LoginLayout, self).__init__(**kwargs)
+        # Set columns
+        self.cols = 1
+        self.spacing=10
+        self.row_force_default=True
+        self.row_default_height=40
 
-#Ventana / ajustar w y h
-window = Tk()
-window.geometry("500x350")
-# ===========================Titulo de la app===================================== #
-window.title("Hotel Duerme Bien")
+        # Add widgets
+        self.add_widget(Label(text="username: "))
+        self.username = TextInput(multiline=True)
+        self.add_widget(self.username)
 
-# ===========================Login como administrador o gerente (button)===================================== #
-label = Label(window, text= "Login",
-            font=("monospace", 20),
-            padx=20,
-            pady=20)
-label.pack()
+        self.add_widget(Label(text="password: "))
+        self.pwd = TextInput(multiline=False)
+        self.add_widget(self.pwd)
 
-#Botones para ingresar como administrador o gerente
-button1 = Button(window,
-                text="Gerente",
-                font=("monospace", 15),
-                state=ACTIVE,
-                padx=10,
-                pady=10,)
-button1.pack()
+    def press(self, manager):
+        username = self.username.text
+        pwd = self.pwd.text
+        if username == "admin" and pwd == "admin":
+            manager.current = 'main'
+        else:
+            self.username.text = ""
+            self.pwd.text = ""
 
-button2 = Button(window,
-                text="Administrador",
-                font=("monospace", 15),
-                state=ACTIVE,
-                padx=10,
-                pady=10,)
-button2.pack()
+class LoginScreen(Screen):
+    pass
 
-# =========================Final======================================= #
-#Hasta aca se ejecuta todo lo que va en el archivo
-window.mainloop()
+class MainScreen(Screen):
+    pass
+
+class HotelApp(App):
+    def build(self):
+        self.__sm = ScreenManager()
+        self.__sm.add_widget(LoginScreen(name="login"))
+        self.__sm.add_widget(MainScreen(name="main"))
+        return self.__sm
+
+if __name__ == '__main__':
+	HotelApp().run()
