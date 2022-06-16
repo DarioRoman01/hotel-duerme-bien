3792bf45d532ca22e605d5ae428cd7af927eeed3
Dario Roman <darioroman2001@gmail.com>
2022-06-16 10:01:29 -0400

    create utils module for utils functions and classes.

---
+++ server/utils/utils.py
@@ -0,0 +1,5 @@
+class NotFoundError(Exception):
+    """Error custom en caso de que un objeto no sea encontrado en la base de datos"""
+
+class NotCreatedErorr(Exception):
+    """Error custom en caso de que la creacion de un objeto en la base de datos no haya sido creado"""
